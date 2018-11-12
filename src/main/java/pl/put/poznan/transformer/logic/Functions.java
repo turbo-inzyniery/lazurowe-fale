963286dc638600dd6ed49444d4497bbde2f8b71d
invalidcorgi <invalidcorgi@gmail.com>
2018-11-12 18:53:32 +0100

    Integration TextTransformer with ShortcutExtender/Shortener, NumberToWord, add logging

--- src/main/java/pl/put/poznan/transformer/logic/Functions.java
+++ src/main/java/pl/put/poznan/transformer/logic/Functions.java
@@ -14,12 +14,10 @@ public class Functions {
     public static String Capitalize(String text){
         String[] string_tab= text.split(" ");
         text = "";
-        System.out.println(string_tab);
         for(String text_fragment : string_tab){
             if (text_fragment.length() > 0) {
                 text_fragment = text_fragment.substring(0, 1).toUpperCase() + text_fragment.substring(1);
                 text += text_fragment + " ";
-                System.out.println(text);
             }
         }
         return text;
--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -1,10 +1,15 @@
 package pl.put.poznan.transformer.logic;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Can apply tranforms to given text
  */
 public class TextTransformer {
 
+    private static final Logger logger = LoggerFactory.getLogger(TextTransformer.class);
     private final String[] transforms;
 
     /**
@@ -21,25 +26,39 @@ public class TextTransformer {
      * @return text after transforms
      */
     public String applyTransformations(String text){
+        logger.debug("Text to transform: "+text);
+        logger.debug("Transforms to apply:");
+        for (String transform:
+             transforms) {
+            logger.debug(transform);
+        }
         for(String operation : this.transforms){
             switch(operation){
-                case "upper":   text = text.toUpperCase();
-                                break;
-                case "lower":   text = text.toLowerCase();
-                                break;
+                case "upper":
+                    text = text.toUpperCase();
+                    break;
+                case "lower":
+                    text = text.toLowerCase();
+                    break;
                 case "capitalize":
-                                text = Functions.Capitalize(text);
-                                break;
-                case "inverse": text = Functions.inverse(text);
-                                break;
-                case "letter":
-                                break;
-                case "short":
-                                break;
-                case "long":
-                                break;
+                    text = Functions.Capitalize(text);
+                    break;
+                case "inverse":
+                    text = Functions.inverse(text);
+                    break;
+                case "extend":
+                    text = ShortcutExtender.Transform(text);
+                    break;
+                case "shorten":
+                    text = ShortcutShortener.Transform(text);
+                    break;
+                case "numberToWord":
+                    text = NumberToWord.transform(text);
+                    break;
             }
+            logger.debug("Text after transform '"+operation+"': "+text);
         }
+        logger.info("Returned text after transforms: "+text);
         return text;
     }
 }
