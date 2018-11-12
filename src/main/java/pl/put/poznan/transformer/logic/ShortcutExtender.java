d716941b111a0e4cc616703fc903d171ed7f717c
Taro94 <stjasiewicz@gmail.com>
2018-11-12 10:09:37 +0100

    Merge

--- src/main/java/pl/put/poznan/transformer/logic/ShortcutExtender.java
+++ src/main/java/pl/put/poznan/transformer/logic/ShortcutExtender.java
@@ -1,4 +1,36 @@
 package pl.put.poznan.transformer.logic;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Set;
+
 public class ShortcutExtender {
+
+    private static final Map<String, String> dic;
+    static {
+        HashMap<String, String> hmap = new HashMap<>();
+        hmap.put("prof.", "profesor");
+        hmap.put("Prof.", "Profesor");
+        hmap.put("PROF.", "PROFESOR");
+        hmap.put("dr", "doktor");
+        hmap.put("Dr", "Doktor");
+        hmap.put("DR", "DOKTOR");
+        hmap.put("np.", "na przykład");
+        hmap.put("Np.", "Na przykład");
+        hmap.put("NP.", "NA PRZYKŁAD");
+        hmap.put("itd.", "i tym podobne");
+        hmap.put("Itd.", "I tym podobne");
+        hmap.put("ITD.", "I TYM PODOBNE");
+        dic = Collections.unmodifiableMap(hmap);
+    }
+
+    public static String Transform(String text)
+    {
+        for ( String key : dic.keySet() ) {
+            text = text.replaceAll(key, dic.get(key));
+        }
+        return text;
+    }
 }
--- src/main/java/pl/put/poznan/transformer/logic/ShortcutShortener.java
+++ src/main/java/pl/put/poznan/transformer/logic/ShortcutShortener.java
@@ -1,12 +1,28 @@
 package pl.put.poznan.transformer.logic;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.Set;
 
-public class ShortcutExtender extends ShortcutTransform {
+public class ShortcutShortener {
+
+    private static final Map<String, String> dic;
+    static {
+        HashMap<String, String> hmap = new HashMap<>();
+        hmap.put("profesor", "prof.");
+        hmap.put("Profesor", "Prof.");
+        hmap.put("PROFESOR", "PROF.");
+        hmap.put("doktor", "dr");
+        hmap.put("Doktor", "Dr");
+        hmap.put("DOKTOR", "DR");
+        hmap.put("na przykład", "np.");
+        hmap.put("Na przykład", "Np.");
+        hmap.put("NA PRZYKŁAD", "NP.");
+        hmap.put("i tym podobne", "itd.");
+        hmap.put("I tym podobne", "Itd.");
+        hmap.put("I TYM PODOBNE", "ITD.");
+        dic = Collections.unmodifiableMap(hmap);
+    }
 
     public static String Transform(String text)
     {
