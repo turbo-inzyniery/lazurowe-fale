147a32e2bb41ca76fe8ea1ff0765661ec9a2de3b
invalidcorgi <invalidcorgi@gmail.com>
2018-11-12 20:06:16 +0100

    Fix: inverse transform now supports unicode characters that couldn't be stored as one char

--- src/main/java/pl/put/poznan/transformer/logic/Functions.java
+++ src/main/java/pl/put/poznan/transformer/logic/Functions.java
@@ -1,5 +1,8 @@
 package pl.put.poznan.transformer.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Contains methods to capitalize and inverse string
  */
@@ -24,36 +27,40 @@ public class Functions {
     }
 
     /**
-     * Inverses text
+     * Inverses text keeping upper cases at original position
      *
      * @param text text string to inverse
      * @return text string after inverse
      */
     public static String inverse(String text){
-        char[] char_array = text.toCharArray();
-        boolean [] upper_signature;
-        upper_signature = new boolean[text.length()];
-        int i = 0;
-        int text_length = text.length();
-        text = "";
+        List<Integer> codePoints = new ArrayList<>();
+        text.codePoints().forEach(point -> codePoints.add(point));
 
-        for(i = 0; i < text_length; i++){
-            if(Character.isUpperCase(char_array[i])){
-                upper_signature[i] = true;
+        List<char[]> charPairs = new ArrayList<>();
+        codePoints.forEach(point -> charPairs.add(Character.toChars(point)));
+
+        boolean [] isUpper = new boolean[charPairs.size()];
+        for (int i = 0; i < charPairs.size(); i++){
+            if(charPairs.get(i).length != 1){
+                isUpper[i] = false;
+                continue;
             }
-            else{
-                upper_signature[i] = false;
+            isUpper[i] = Character.isUpperCase(charPairs.get(i)[0]);
+        }
+
+        StringBuilder textBuilder = new StringBuilder();
+        for (int i = 0, j = charPairs.size()-1; i < isUpper.length; i++, j--){
+            if(charPairs.get(j).length == 1){
+                if(isUpper[i]){
+                    textBuilder.append(Character.toUpperCase(charPairs.get(j)[0]));
+                }else {
+                    textBuilder.append(Character.toLowerCase(charPairs.get(j)[0]));
+                }
+            }else {
+                textBuilder.append(new String(charPairs.get(j)));
             }
         }
 
-        for(i = i - 1 ; i >= 0; i--){
-            if(upper_signature[text_length - (i + 1)]){
-                text += Character.toUpperCase(char_array[i]);
-            }
-            else {
-                text += Character.toLowerCase(char_array[i]);
-            }
-        }
-        return text;
+        return textBuilder.toString();
     }
 }
