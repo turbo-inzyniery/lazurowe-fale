aa595a89e6b03dc9ef559b1611f765230d8c4cfe
Johny <j.dudziak97@gmail.com>
2018-12-11 14:12:49 +0100

    Add unit tests

--- src/test/java/pl/put/poznan/transformer/logic/LatexTransformerTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/LatexTransformerTest.java
@@ -1,6 +1,4 @@
 package pl.put.poznan.transformer.logic;
-
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,15 +7,43 @@ public class LatexTransformerTest {
     static Transformer transformer;
     static String text;
 
-    @Before
-    public void setUp(){
+    @Test
+    public void transform1() {
         transformer = new BaseTransformer(" \\& \\& ");
         transformer = new LatexTransformer(transformer);
         text = transformer.transform();
+        assertEquals(" & & ", text );
     }
 
     @Test
-    public void transform1() {
-        assertEquals(" & & ", text );
+    public void transform2() {
+        transformer = new BaseTransformer("\\& dsfbskfdsjgss\\$\\");
+        transformer = new LatexTransformer(transformer);
+        text = transformer.transform();
+        assertEquals("& dsfbskfdsjgss$\\", text );
     }
+
+    @Test
+    public void transform3() {
+        transformer = new BaseTransformer("\\$ \\& dansjdaf\\$sadfs\\& ");
+        transformer = new LatexTransformer(transformer);
+        text = transformer.transform();
+        assertEquals( "$ & dansjdaf$sadfs& ", text );
+    }
+
+    @Test
+    public void transform4() {
+        transformer = new BaseTransformer("\\&\\$\\&\\$");
+        transformer = new LatexTransformer(transformer);
+        text = transformer.transform();
+        assertEquals("&$&$", text );
+    }
+    @Test
+    public void transform5() {
+        transformer = new BaseTransformer("123\\&321\\&\\$\\$");
+        transformer = new LatexTransformer(transformer);
+        text = transformer.transform();
+        assertEquals("123&321&$$", text );
+    }
+
 }
---
+++ src/test/java/pl/put/poznan/transformer/logic/ReplicRemovalTest.java
@@ -0,0 +1,50 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReplicRemovalTest {
+    static Transformer transformer;
+    static String text;
+
+    @Test
+    public void transformReplicRemoval1() {
+        transformer = new BaseTransformer("do do do do do do");
+        transformer = new ReplicRemoval(transformer);
+        text = transformer.transform();
+        assertEquals("do", text );
+    }
+
+    @Test
+    public void transformReplicRemoval2() {
+        transformer = new BaseTransformer("do do");
+        transformer = new ReplicRemoval(transformer);
+        text = transformer.transform();
+        assertEquals("do", text );
+    }
+
+    @Test
+    public void transformReplicRemoval3() {
+        transformer = new BaseTransformer("123456789 123456789 123456789");
+        transformer = new ReplicRemoval(transformer);
+        text = transformer.transform();
+        assertEquals("123456789", text );
+    }
+
+    @Test
+    public void transformReplicRemoval4() {
+        transformer = new BaseTransformer("2 2 2 2 2 2 2");
+        transformer = new ReplicRemoval(transformer);
+        text = transformer.transform();
+        assertEquals("2", text );
+    }
+
+    @Test
+    public void transformReplicRemoval5() {
+        transformer = new BaseTransformer("#$% #$%");
+        transformer = new ReplicRemoval(transformer);
+        text = transformer.transform();
+        assertEquals("#$%", text );
+    }
+}
